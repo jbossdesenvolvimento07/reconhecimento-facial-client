073879a23d49d7180f90e0dc632b4d61035371f6
JBOSS - Vitor Bersani <desenvolvimento07@jboss.srv.br>
2022-02-22 17:37:08 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,9 +15,9 @@ function Header() {
 
             <Collapse navbar isOpen={collapse} >
                 <Nav navbar className='ps-3'>
-                    <NavLink to="/validacao" className="NavbarLink my-2 m-md-0 me-md-3"><i class="bi bi-check-square me-2"></i>Validação </NavLink>
-                    <NavLink to="/cadastro" className="NavbarLink my-2 m-md-0 me-md-3"><i class="bi bi-plus-square me-2"></i>Cadastro </NavLink>
-                    <NavLink to="/configuracao" className="NavbarLink mt-4 mb-2 m-md-0"><i class="bi bi-gear me-2"></i>Configuração </NavLink>
+                    <NavLink to="/validacao" className="NavbarLink my-2 m-md-0 me-md-3"><i className="bi bi-check-square me-2"></i>Validação </NavLink>
+                    <NavLink to="/cadastro" className="NavbarLink my-2 m-md-0 me-md-3"><i className="bi bi-plus-square me-2"></i>Cadastro </NavLink>
+                    <NavLink to="/configuracao" className="NavbarLink mt-4 mb-2 m-md-0"><i className="bi bi-gear me-2"></i>Configuração </NavLink>
                 </Nav>
             </Collapse>
         </Navbar>
